89796aa6833337a77fd3f8e75c583dda7ad07445
Sean Walker <walker.se@northeastern.edu>
2026-05-15 00:59:43 -0400

    chore: add Vitest setup with jsdom and fake-indexeddb

---
+++ tests/setup.ts
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom/vitest';
+import 'fake-indexeddb/auto';
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+
+afterEach(() => {
+  cleanup();
+});
